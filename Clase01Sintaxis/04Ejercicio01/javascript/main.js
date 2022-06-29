2319cdca69aa9eb6f5e04410abdeeee7831c799a
sebaroi <103544187+sebaroi@users.noreply.github.com>
2022-06-29 09:46:14 -0300

    Java Script

---
+++ Clase01Sintaxis/04Ejercicio01/javascript/main.js
@@ -0,0 +1,20 @@
+
+ /*
+    Escriba un programa que guard la palabra
+
+    "SUMA"
+   
+    en una variable y los números 3 y 5 en otras variables. 
+
+    Finalmente, imprima en consola el resultado de la operación usando las variables 
+    donde tiene almacenados el 3 y el 5. 
+
+ */
+    let palabra = "suma"
+
+    let primerNumero = 3
+    let segundoNumero = 5
+    const resultado = primerNumero + segundoNumero
+    
+    console.log(palabra, resultado)
+    
---
+++ Clase01Sintaxis/06Ejercicio2/javascript/main.js
@@ -0,0 +1,28 @@
+
+/*
+    Escriba un programa que le pida a un usuario 2 números y muestre:
+
+    A. El resultado de la suma en la consola
+    B. El resultado de la multiplicaión en un alert
+
+
+    Debe indicarle al usuario la operación ejecutada. 
+
+    Por ejemplo, si el usuario ingresa los números 8 y 10
+
+    Debe mostrar un alert que diga: 
+    "La multiplicación de los números ingresados es: 80"
+
+    Y en la consola: 
+    "La suma de los números ingresados es: 18
+
+*/
+
+
+
+  let numero1 = Number(prompt("Ingrese un numero"))
+  let numero2 = Number(prompt("Ingrese otro numero"))
+  let suma = numero1+numero2
+  let mul = numero1*numero2
+  alert(`Multiplicación ${mul}`)
+  console.log(`Suma, ${suma}`)
