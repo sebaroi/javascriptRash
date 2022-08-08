c616054482dd1a43ec81a9d53ddd0378cc250e4a
sebasroi <sroizenberg@gmail.com>
2022-08-07 22:44:02 -0300

    desarrolando carrito compras

--- backoffice/js/main.js
+++ backoffice/js/main.js
@@ -14,28 +14,115 @@ const catalogoProdu = new CatalogoProdu(productos)
 
 const categorias = ["placas de Video", "mother", "Notebooks", "monitores" ]
 
+
+//listarProducto()
+//listarPro()   para listar los prodcutos cuanto entro con admin adminn
+ingProd()
+
+modPro()
+busPro()
+
+
+
+
+
+const nodoPrincipal = document.getElementById("contenedorProductos")
+
+
 const contenedorCarrito = document.getElementById('carritoContenedor')
+
 const botonVaciar = document.getElementById('vaciarCarrito')
 
+const contadorCarrito = document.getElementById('contadorCarrito')
+
+const cantidad = document.getElementById('cantidad')
+const precioTot = document.getElementById('precioTotal')
+const cantidadTotal = document.getElementById('cantidadTotal')
+
 let carrito = []
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        actualizarCarrito()
+    }
+})
 
 botonVaciar.addEventListener('click', () => {
     carrito.length = 0
     actualizarCarrito()
 })
 
-ingProd()
-listarPro()
-modPro()
-busPro()
-fin()
 
+catalogoProdu.productos.forEach((producto)=>{
+    const divProducto = document.createElement("div")
+    divProducto.className=" col-lg-3 col-md-4 col-sm-6 "
+    divProducto.innerHTML=`
+                <div class="marcoProducto">
+                    <div class="imagenProducto">
+                        <img src=${producto.imagen}>
+                    </div>
+                    <div class="textoProducto">
+                        <p>${producto.nombreProdu}</p>
+                        <p>$${producto.precio}</p>
+                        <hr>
+                        <div class="botonComprar" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            <button id="agregar${producto.sku}" class="button" <i class="fas fa-shopping-cart"></i>COMPRAR</button><i class="fas fa-shopping-cart"></i>
+                        </div>
+                    </div>
+                </div> `
+    nodoPrincipal.appendChild(divProducto);
+    //document.body.appendChild()
+    let element= `agregar${producto.sku}`
 
-    const btn = document.getElementById("botonCrearProd")
-    btn.addEventListener("click", (e)=>{
-        cargarDatosProductos(e)
-        alert("producto ingresado")
+    const botonAgregar = document.getElementById(element)
+    botonAgregar.addEventListener('click', () => {
+    agregarAlCarrito(producto.sku)
     })
+})
+
+
+function agregarAlCarrito (prodId)  {
+    const item = catalogoProdu.productos.find((producto) => producto.sku === prodId)
+    carrito.push(item)
+    actualizarCarrito()
+    console.log(carrito)
+}
+
+
+const eliminarDelCarrito = (prodId) => {
+    const Item = carrito.find((producto) => producto.sku === prodId)
+    const indice = carrito.indexOf(item)
+    carrito.splice(indice,1)
+    actualizarCarrito()
+}
+
+
+
+const actualizarCarrito = () => {
+    contenedorCarrito.innerHTML=""
+    carrito.forEach((producto) => {
+        const div = document.createElement('div')
+        div.className = ('productoEnCarrito')
+        div.innerHTML = `
+        console.log(producto.nombreProdu)
+        <p>${producto.nombreProdu}</p>
+        <p>${producto.imagen}</p>
+        <p>Precio:$ ${producto.precio}</p>
+        <p>cantidad: <span id="cantidad">${producto.stock}</span> </p>
+        <button onclick = "eliminarDelCarrito((${producto.sku})" class="botonComprar"><i class="fas fa-trash-alt"></button>
+
+        `
+        contenedorCarrito.appendChild(div)
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+    })
+    contenedorCarrito.innerHTML = carrito.length
+    console.log(carrito)
+    precioTot.innerText = carrito.reduce((acc, producto) =>/* acc + producto.cantidad **/ producto.precio, 0)
+}
+
+
 
 
 function ingProd()
@@ -51,8 +138,6 @@ function ingProd()
  const adminNode = document.getElementById("CargaProd")
  adminNode.appendChild(btn); //cuando oculeto me muestro los otros botones
 }
-
-
 
 
 function cargarDatosProductos(e)
@@ -78,10 +163,19 @@ function cargarDatosProductos(e)
     descri = prompt("Ingrese la Descripción: ");
     cantidad = prompt("Ingrese la cantidad en stock: ");
     imag = prompt("Ingrese la imagen: ");*/
-
 }
 
+/*
+    const btn = document.getElementById("botonAgregar")
+    btn.addEventListener("click", () =>{
+        cargarDatosProductos()
+        alert("producto ingresado")
+    })
 
+*/
+
+
+/*
 function listarPro()
 {
     const btn = document.createElement("button")
@@ -94,6 +188,8 @@ function listarPro()
     const adminNode = document.getElementById("Mostrar")
     adminNode.appendChild(btn);  
 }
+*/
+
 
 function modPro()
 {
@@ -124,23 +220,6 @@ function busPro()
 
 
 
-function fin()
-{
-    const btn = document.createElement("button")
-    btn.innerText="SALIR"
-    btn.classList.add("button")
-    btn.addEventListener("click", ()=>
-    {
-        salir();
-    });
-    const adminNode = document.getElementById("Finalizar")
-    adminNode.appendChild(btn);  
-}
-
-function salir (){
-    alert("Saludos");
-
-}
 
 
 
@@ -167,6 +246,19 @@ function modificar () {
 
 
 
+    
+    
+ 
+
+
+
+
+
+
+
+
+
+/*
 function listarProducto() {
 
     const nodoPrincipal = document.getElementById("contenedorProductos")
@@ -191,50 +283,16 @@ function listarProducto() {
                     </div> `
         nodoPrincipal.appendChild(divProducto);
         //document.body.appendChild()
+        let element= `agregar${producto.sku}`
 
-        const botonAgregar = document.getElementById('agregar${producto.sku}')
-        boton.addEventListener('click', () => {
+        const botonAgregar = document.getElementById(element)
+        botonAgregar.addEventListener('click', () => {
         agregarAlCarrito(producto.sku)
         })
     })
 }
 
-
-
-const agregarAlCarrito = (prodId) => {
-    const Item = catalogoProdu.productos.find((producto) => producto.sku === prodId)
-    carrito.push(item)
-    actualizarCarrito()
-    console.log(carrito)
-}
-
-
-const eliminarDelCarrito = (prodId) => {
-    const Item = carrito.find((producto) => producto.sku === prodId)
-    const indice = carrito.indexOf(item)
-    carrito.splice(indice,1)
-    actualizarCarrito()
-}
-
-
-
-const actualizarCarrito = () => {
-    contenedorCarrito.innerHTML=""
-    carrito.forEach((producto) => {
-        const div = document.createElement('div')
-        //aca iria el css del carrito div.className= ('el nombre de la clase del css')
-        div.innerHTML = `
-        <p>${producto.nombreProdu}</p>
-        <p>Precio:$ ${producto.precio}</p>
-        <p>cantidad: <span id="cantidad">${producto.stock}</span> </p>
-        <button onclick = "eliminarDelCarrito((${producto.sku})" class="botonComprar"><i class="fas fa-trash-alt"></button>
-
-        `
-        contenedorCarrito.appendChild(div)
-
-    })
-    contenedorCarrito.innerHTML = carrito.length
-}
+*/
 
 
 /*
